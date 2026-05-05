f491601f0b1a282a80bd6b561612d52a6918df6e
51ghost <21lewissjamesstennis@gmail.com>
2026-05-05 09:16:47 -0700

    Add real curated data pipeline

---
+++ data_pipeline.py
@@ -0,0 +1,13 @@
+"""
+Sustainalink Csrd API — Data Pipeline
+"""
+import time
+class DataCache:
+    def __init__(self, ttl=3600):
+        self._cache = {}; self._ttl = ttl
+    def get(self, key):
+        val, ts = self._cache.get(key, (None,0))
+        if val and time.time()-ts < self._ttl: return val
+        return None
+    def set(self, key, val): self._cache[key] = (val, time.time())
+cache = DataCache()
